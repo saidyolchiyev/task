2e5d71ca57db3f059fadc019ccac1387f0081a03
Said Yolciyev <said14y2007@gmail.com>
2024-10-31 18:43:26 +0400

    31.10.24

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const random = Math.floor(Math.random()*5+95)
-console.log(random)
+// task 1
+let name = prompt('Enter your name')
+console.log(name)
+
+// task 2
+let num1 = parseInt(prompt('number 1'))
+let num2 = parseInt(prompt('number 2'))
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+// Task 3
+let num = 12345*98765
+console.log(num.toString()[5])
